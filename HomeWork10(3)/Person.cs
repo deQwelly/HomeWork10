b77702eebfbc986ad003871e94a6f5756a7279e5
deQwelly <144365731+deQwelly@users.noreply.github.com>
2023-11-30 23:56:28 +0300

    Add files via upload

---
+++ HomeWork10(3)/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork10_3_
+{
+    public class Person
+    {
+        public delegate void NeedfulEvent(string e);
+        public event NeedfulEvent ReactionToEvent;
+
+        public readonly static List<Person> all = new List<Person>();
+        public string Name { get; private set; }
+        public string WhatAwaits { get; private set; }
+
+        public Person(string name, string whatAwaits)
+        {
+            Name = name;
+            WhatAwaits = whatAwaits;
+            all.Add(this);
+        }
+
+        public void React()
+        {
+            ReactionToEvent.Invoke($"{Name} очень рад событию {WhatAwaits}");
+        }
+    }
+}
--- HomeWork10(3)/Program.cs
+++ HomeWork10(3)/Program.cs
@@ -8,9 +8,49 @@ namespace HomeWork10_3_
 {
     internal class Program
     {
+        public static void DisplayMassage(string mes)
+        {
+            Console.WriteLine(mes);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Выберите одно из следующих событий");
+            string[] evs = { "Новая глава манги", "Новый фильм Нолана", "Выход новой видеокарты", "Новая столовая рядом с унивром", "На пару принесли конфетки" };
+            for (int i = 0; i < evs.Length; i++)
+            {
+                Console.WriteLine($"{i+1} - {evs[i]}");
+            }
+            Console.Write("Напишите номер понравившегося события: ");
+            if (int.TryParse(Console.ReadLine(), out int ev))
+            {
+                if (ev > 0 || ev < (evs.Length + 1))
+                {
+                    Person p1 = new Person("Саша", "Новая глава манги");
+                    Person p2 = new Person("Дима", "Новый фильм Нолана");
+                    Person p3 = new Person("Ваня", "Новая столовая рядом с унивром");
 
+                    p1.ReactionToEvent += DisplayMassage;
+                    p2.ReactionToEvent += DisplayMassage;
+                    p3.ReactionToEvent += DisplayMassage;
+
+                    foreach (Person p in Person.all)
+                    {
+                        if (p.WhatAwaits == evs[ev])
+                        {
+                            p.React();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Указанное число не принадлежит номеру");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введен номер");
+            }
         }
     }
 }
